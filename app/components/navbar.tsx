114d8104fbb3fa6d73ff7b2d2816ddf2ab932151
luibelzan <luismi_madrilista@hotmail.com>
2024-11-21 17:04:52 +0100

    Uploading starter

---
+++ app/components/navbar.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import Link from 'next/link';
+import { useState } from 'react';
+
+const NavBar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => setIsOpen(!isOpen);
+
+    return (
+        <nav className="bg-gray-800 p-4">
+            <div className="container mx-auto flex justify-between items-center">
+                {/* Logo or name */}
+                <Link href="/">
+                    Softforge Solutions
+                </Link>
+        
+                {/* Menu button for mobile */}
+                <div className="md:hidden">
+                    <button onClick={toggleMenu} className="text-white">
+                        {isOpen ? 'Cerrar' : 'Menú'}
+                    </button>
+                </div>
+        
+                {/* Main menu - Mobile and Desktop */}
+                <ul className={`space-x-6 ${isOpen ? 'block' : 'hidden'} md:flex`}>
+                    <li>
+                        <Link href="/">Inicio</Link>
+                    </li>
+                    <li>
+                        <Link href="/about">Sobre mí</Link>
+                    </li>
+                    <li>
+                        <Link href="/projects">Proyectos</Link>
+                    </li>
+                    <li>
+                        <Link href="/contact">Contacto</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+    };
+
+export default NavBar;
