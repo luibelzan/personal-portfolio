451f6e21fcadd025b9352ee787fdd7778e05ecf9
luibelzan <luismi_madrilista@hotmail.com>
2024-12-18 16:12:12 +0100

    Project description added

--- app/project/funky-art/page.tsx
+++ app/project/funky-art/page.tsx
@@ -4,14 +4,22 @@ export default async function Page({params,}: {params: Promise<{ slug: string }>
     const project = (await params).slug
     return (
         <div className="project-page w-full">
-            <div className="container mx-auto flex justify-between items-center flex-col m-auto overflow-hidden">
+            <div className="container mx-auto flex items-center flex-col m-auto overflow-hidden">
+                <h1 className="project-name text-5xl font-extrabold text-center text-gray-800 tracking-tight leading-tight mt-12 pb-3">Funky Art</h1>
                 <div className="box">
                     <span className={styles['mi-clase']}></span>
                     <span className={styles['mi-clase']}></span>
                     <span className={styles['mi-clase']}></span>
                     <span className={styles['mi-clase']}></span>
                 </div>
-                <h1 className="pb-14 mb-8 text-4xl font-bold text-center text-black">Funky Art</h1>
+
+                <div className="flex justify-center flex-col items-center">
+                    <h2 className="project-description">Plataforma Web3 para acu;ar activos digitales en la Blockchain de Ethereum</h2>
+                    <div className="flex flex-row">
+                        <p>texto</p>
+                    </div>
+                </div>
+                
             </div>
         </div>
     )
