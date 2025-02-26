382eb4949b002f0c2b559be48aa9d3b3deeb5545
Luis Miguel <luibelzan@outlook.com>
2025-02-26 20:52:01 +0100

    Responsive funky art page

--- src/app/[locale]/project/funky-art/page.tsx
+++ src/app/[locale]/project/funky-art/page.tsx
@@ -7,8 +7,8 @@ export default function FunkyArt() {
     return (
         <div className="project-page w-full">
             <div className="container flex items-center flex-col m-auto overflow-hidden py-16 px-8">
-                <h1 className="project-name mt-12 pb-3">Funky Art</h1>
-                <div className="box shared-style">
+                <h1 className="project-name mt-6 md:mt-0 pb-2 md:pb-3 text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold max-md:mt-0">Funky Art</h1>
+                <div className="box shared-style md:w-full mt-28 max-md:mt-10">
                     <span className={styles['mi-clase']}></span>
                     <span className={styles['mi-clase']}></span>
                     <span className={styles['mi-clase']}></span>
@@ -16,10 +16,10 @@ export default function FunkyArt() {
                 </div>
 
                 <div className="flex justify-center flex-col items-center">
-                    <h2 className="project-short-description pb-4">{t('funkyArtShortDescription')}</h2>
+                    <h2 className="project-short-description pb-4 max-md:text-xl sm:text-2xl md:text-5xl max-md:pt-9 max-md:pb-9 md:pt-40 font-semibold text-center">{t('funkyArtShortDescription')}</h2>
                     <div className="flex flex-col items-center">
                         <img className="collage" src="/funkyart/collage5.png"></img>\
-                        <p className="project-description text-black">{t('funkyArtLongDescription')}</p>
+                        <p className="project-description text-xl max-md:text-sm max-md:text-center text-start pb-4 text-black">{t('funkyArtLongDescription')}</p>
                         <img src="/funkyart/nft.png" className="pb-20"></img>
                     </div>
                 </div>
